a640af1d2dbfe9f2c53834b2d62c81ce073df5dc
Nageshwar Reddy Pandem <nageshwar521@Nageshwars-MacBook-Air.local>
2021-06-23 20:03:38 +0530

    feat: express api boilerplate

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const socketIo = require("socket.io");
+const { initDB } = require("./app/db/connection");
+const authRoutes = require("./app/routes/auth");
+const usersRoutes = require("./app/routes/users");
+const { verifyToken } = require("./app/middlewares/auth");
+
+require("dotenv").config();
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIo(server).sockets;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", verifyToken, usersRoutes);
+
+require("./app/middlewares/socket")(io);
+
+app.listen(5000, () => {
+  initDB();
+  console.log("Server running at http://localhost:5000");
+});
